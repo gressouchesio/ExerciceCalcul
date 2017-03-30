1713ebe81c33343a859f7baae0302c173bcaf7e0
RESSOUCHE <RESSOUCHE@P28-PROFS>
2017-03-30 16:17:51 +0200

    Ajoutez des fichiers projet.

---
+++ SIO1-ProfDeCalcul/frmJeu.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SIO1_ProfDeCalcul
+{
+    partial class frmJeu
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJouer = new System.Windows.Forms.Button();
+            this.rbtnL1 = new System.Windows.Forms.RadioButton();
+            this.rbtnL2 = new System.Windows.Forms.RadioButton();
+            this.rbtnL3 = new System.Windows.Forms.RadioButton();
+            this.grpSolutions = new System.Windows.Forms.GroupBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblExercice = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fichierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpSolutions.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnJouer
+            // 
+            this.btnJouer.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJouer.Location = new System.Drawing.Point(68, 72);
+            this.btnJouer.Name = "btnJouer";
+            this.btnJouer.Size = new System.Drawing.Size(230, 34);
+            this.btnJouer.TabIndex = 0;
+            this.btnJouer.Text = "JOUER";
+            this.btnJouer.UseVisualStyleBackColor = true;
+            this.btnJouer.Click += new System.EventHandler(this.btnJouer_Click);
+            // 
+            // rbtnL1
+            // 
+            this.rbtnL1.AutoSize = true;
+            this.rbtnL1.Location = new System.Drawing.Point(20, 19);
+            this.rbtnL1.Name = "rbtnL1";
+            this.rbtnL1.Size = new System.Drawing.Size(14, 13);
+            this.rbtnL1.TabIndex = 1;
+            this.rbtnL1.TabStop = true;
+            this.rbtnL1.UseVisualStyleBackColor = true;
+            // 
+            // rbtnL2
+            // 
+            this.rbtnL2.AutoSize = true;
+            this.rbtnL2.Location = new System.Drawing.Point(20, 48);
+            this.rbtnL2.Name = "rbtnL2";
+            this.rbtnL2.Size = new System.Drawing.Size(14, 13);
+            this.rbtnL2.TabIndex = 2;
+            this.rbtnL2.TabStop = true;
+            this.rbtnL2.UseVisualStyleBackColor = true;
+            // 
+            // rbtnL3
+            // 
+            this.rbtnL3.AutoSize = true;
+            this.rbtnL3.Location = new System.Drawing.Point(20, 81);
+            this.rbtnL3.Name = "rbtnL3";
+            this.rbtnL3.Size = new System.Drawing.Size(14, 13);
+            this.rbtnL3.TabIndex = 3;
+            this.rbtnL3.TabStop = true;
+            this.rbtnL3.UseVisualStyleBackColor = true;
+            // 
+            // grpSolutions
+            // 
+            this.grpSolutions.Controls.Add(this.rbtnL1);
+            this.grpSolutions.Controls.Add(this.rbtnL3);
+            this.grpSolutions.Controls.Add(this.rbtnL2);
+            this.grpSolutions.Location = new System.Drawing.Point(68, 218);
+            this.grpSolutions.Name = "grpSolutions";
+            this.grpSolutions.Size = new System.Drawing.Size(230, 115);
+            this.grpSolutions.TabIndex = 4;
+            this.grpSolutions.TabStop = false;
+            this.grpSolutions.Text = "Solution";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(68, 379);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(230, 44);
+            this.btnOK.TabIndex = 5;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblExercice
+            // 
+            this.lblExercice.AutoSize = true;
+            this.lblExercice.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExercice.Location = new System.Drawing.Point(73, 152);
+            this.lblExercice.Name = "lblExercice";
+            this.lblExercice.Size = new System.Drawing.Size(0, 24);
+            this.lblExercice.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fichierToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fichierToolStripMenuItem
+            // 
+            this.fichierToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitterToolStripMenuItem});
+            this.fichierToolStripMenuItem.Name = "fichierToolStripMenuItem";
+            this.fichierToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.fichierToolStripMenuItem.Text = "Fichier";
+            // 
+            // quitterToolStripMenuItem
+            // 
+            this.quitterToolStripMenuItem.Name = "quitterToolStripMenuItem";
+            this.quitterToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.quitterToolStripMenuItem.Text = "Quitter";
+            this.quitterToolStripMenuItem.Click += new System.EventHandler(this.quitterToolStripMenuItem_Click);
+            // 
+            // frmJeu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 455);
+            this.Controls.Add(this.lblExercice);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.grpSolutions);
+            this.Controls.Add(this.btnJouer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmJeu";
+            this.Text = "Prof de calcul";
+            this.Load += new System.EventHandler(this.frmJeu_Load);
+            this.grpSolutions.ResumeLayout(false);
+            this.grpSolutions.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJouer;
+        private System.Windows.Forms.RadioButton rbtnL1;
+        private System.Windows.Forms.RadioButton rbtnL2;
+        private System.Windows.Forms.RadioButton rbtnL3;
+        private System.Windows.Forms.GroupBox grpSolutions;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblExercice;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fichierToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitterToolStripMenuItem;
+    }
+}
+
---
+++ SIO1-ProfDeCalcul/frmJeu.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SIO1_ProfDeCalcul
+{
+    public partial class frmJeu : Form
+    {
+        public frmJeu()
+        {
+            InitializeComponent();
+        }
+        //=====================================================
+
+        # region VARIABLES GLOBALES
+
+        public static int resultat;
+        public static int resultatAlt1;
+        public static int resultatAlt2;
+        public static string exercice;
+        public static Random rndm;
+
+        # endregion
+
+        //=====================================================
+
+        # region PROCEDURES EVENEMENTIELLES
+
+        private void frmJeu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void btnJouer_Click(object sender, EventArgs e)
+        {
+            // Clic sur le bouton JOUER :
+            // on crée l'exercice (l'opération + les réponses possibles)
+            creerExercice();
+            proposerSolutions();
+        }
+
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            // Clic sur le bouton OK :
+            // Affichage de MessageBox en fonction de la réponse sélectionnée
+
+            if (rbtnL1.Checked && rbtnL1.Text == resultat.ToString())
+            {
+                MessageBox.Show("BONNE REPONSE !");
+            }
+            else
+            {
+                if (rbtnL2.Checked && rbtnL2.Text == resultat.ToString())
+                {
+                    MessageBox.Show("BONNE REPONSE !");
+                }
+                else
+                {
+                    if (rbtnL3.Checked && rbtnL3.Text == resultat.ToString())
+                    {
+                        MessageBox.Show("BONNE REPONSE !");
+                    }
+                    else
+                    {
+
+                        MessageBox.Show("ERREUR");
+                    }
+                }
+            }
+        }
+
+
+        private void quitterToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        # endregion
+
+        //=====================================================
+
+        # region PROCEDURES/FONCTIONS UTILISATEURS
+
+        // Création de l'exercice
+        private void creerExercice()
+        {
+            // on détermine aléatoirement les 2 membres de l'opération
+            rndm = new Random();
+            int nombre1 = rndm.Next(1, 21);
+            int nombre2 = rndm.Next(1, 21);
+
+            // on met en forme le label
+            exercice = nombre1.ToString() + " + " + nombre2.ToString() + " = ";
+            lblExercice.Text = exercice;
+
+            // on calcule le résultat réel de l'opération
+            resultat = nombre1 + nombre2;
+        }
+
+
+        // Recherche de 2 résultats alternatifs, différents du vrai résultat
+        private void proposerSolutions()
+        {
+            // Le résultat a été calculé, il faut calculer les 2 résultats alternatifs.
+            // Pour cela, on leur affecte une valeur aléatoire différente du résultat réel
+
+            resultatAlt1 = resultat;
+            resultatAlt2 = resultat;
+
+            while (resultat == resultatAlt1)
+            {
+                resultatAlt1 = (rndm.Next(2, 41));
+            }
+
+            while (resultat == resultatAlt2 || resultatAlt1 == resultatAlt2)
+            {
+                resultatAlt2 = (rndm.Next(2, 41));
+            }
+
+            // On affecte le résultat réel et les 2 résultats alternatifs aux boutons radio
+            // dans l'ordre croissant
+
+            if (resultat < resultatAlt1 && resultat < resultatAlt2)
+            {
+                rbtnL1.Text = resultat.ToString();
+                if (resultatAlt1 < resultatAlt2)
+                {
+                    rbtnL2.Text = resultatAlt1.ToString();
+                    rbtnL3.Text = resultatAlt2.ToString();
+                }
+                else
+                {
+                    rbtnL2.Text = resultatAlt2.ToString();
+                    rbtnL3.Text = resultatAlt1.ToString();
+                }
+            }
+
+            if (resultatAlt1 < resultatAlt2 && resultatAlt1 < resultat)
+            {
+                rbtnL1.Text = resultatAlt1.ToString();
+                if (resultat < resultatAlt2)
+                {
+                    rbtnL2.Text = resultat.ToString();
+                    rbtnL3.Text = resultatAlt2.ToString();
+                }
+                else
+                {
+                    rbtnL2.Text = resultatAlt2.ToString();
+                    rbtnL3.Text = resultat.ToString();
+                }
+            }
+
+            if (resultatAlt2 < resultatAlt1 && resultatAlt2 < resultat)
+            {
+                rbtnL1.Text = resultatAlt2.ToString();
+                if (resultat < resultatAlt1)
+                {
+                    rbtnL2.Text = resultat.ToString();
+                    rbtnL3.Text = resultatAlt1.ToString();
+                }
+                else
+                {
+                    rbtnL2.Text = resultatAlt1.ToString();
+                    rbtnL3.Text = resultat.ToString();
+                }
+            }
+
+            // décochage des bouton radio avant leur affichage
+            rbtnL1.Checked = false;
+            rbtnL2.Checked = false;
+            rbtnL3.Checked = false;
+
+        }
+
+        # endregion
+
+
+    }
+}
